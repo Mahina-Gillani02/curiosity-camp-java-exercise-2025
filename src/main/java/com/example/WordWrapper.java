c5ec741bc3275cc84d61ddbc9e0d6e69f4d8801c
Mahina Gillani <mahina.gillani@autotrader.co.uk>
2025-10-01 12:55:39 +0100

    remove solution

--- src/main/java/com/example/WordWrapper.java
+++ src/main/java/com/example/WordWrapper.java
@@ -3,14 +3,6 @@ package com.example;
 public class WordWrapper {
 
     public String wrap(String text, int columnLength) {
-        //todo THIS IS A TEST MESSAGE
-//        GIVEN SOLUTION
-//        if (text.length() <= columnLength) {
-//            return text;
-//        }
-//        String wrappedText = text.substring(0, columnLength) + '\n';
-//        String remainingText = text.substring(columnLength);
-//        return wrappedText + this.wrap(remainingText, columnLength);
         return "";
     }
 }
